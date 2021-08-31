d472ba8ef070480e759734ec6b19261a74f947eb
Bektemir <bektemirsatybaldiev@gmail.com>
2021-08-31 22:34:42 +0600

    Добавлена игра

---
+++ space_game.py
@@ -0,0 +1,23 @@
+import pygame, controls
+from gun import Gun
+from pygame.sprite import Group
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((500, 600))
+    pygame.display.set_caption("Cosmo")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+    bullets = Group()
+
+    while True:
+        controls.events(screen, gun, bullets)
+        gun.update_gun()
+        controls.update(bg_color, screen, gun, bullets)
+        controls.update_bullets(bullets)
+
+
+
+
+run()
